4f0fbd806adb2c1e4d9b7cc6ce29c32db342a1eb
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2024-07-09 12:02:15 +0530

    remove redundant template config and setup instructions

--- server/scripts/create_git_template.sh
+++ server/scripts/create_git_template.sh
@@ -2,21 +2,12 @@
 
 set -e -o pipefail
 
-# Step 1: Create the template repository
-TEMPLATE_DIR="/tmp/git_template"
 BARE_TEMPLATE_DIR="/srv/git/templates/default"
 
-# Initialize a non-bare repository for the template
-mkdir -pv $TEMPLATE_DIR
-cd $TEMPLATE_DIR
-git init
-git config user.name "dotcodeschool-bot" && git config user.email "hello@dotcodeschool.com"
-echo "Initial commit" > README.md
-git add README.md
-git commit -m "init [skip ci]"
+mkdir -pv $BARE_TEMPLATE_DIR
 
-# Convert to a bare repository
-git clone --bare . $BARE_TEMPLATE_DIR
+# Initialize the bare repository
+git init --bare $BARE_TEMPLATE_DIR
 
 # Set the restrictive settings in the bare repository
 cd $BARE_TEMPLATE_DIR
@@ -28,6 +19,5 @@ git config http.receivepack true
 
 # Clean up the non-bare template directory
 cd $BARE_TEMPLATE_DIR
-rm -rf $TEMPLATE_DIR
 
 echo "Template repository created successfully."
